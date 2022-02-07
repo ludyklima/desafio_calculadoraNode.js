9c98fa391f17f3ccc497044100aedee0ea12b909
Ludmilla Khatarina <94331791+ludyklima@users.noreply.github.com>
2022-02-07 03:15:09 -0300

    Delete Desafio da Calculadora de Módulos com Node.js

--- Desafio da Calculadora de Módulos com Node.js
+++
@@ -1,6 +0,0 @@
-Desafio da Calculadora de Módulos com Node.js
-
--  Monte um pacote de node que faça a leitura de dois números.
--  Depois execute uma escolha entre as 4 operações.
--  Por fim, devolva a operação executada e o resultado de acordo.
--  O pacote deve usar módulos inquirer e um módulo interno.
